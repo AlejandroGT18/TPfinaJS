db392cfe1fe1d13ed77f0cfe689a5a81ec327b56
AlejandroGT18 <gonzalezteseyraalejandro@gmail.com>
2023-11-04 23:26:17 -0300

    Arreglos del footer

--- principal.js
+++ principal.js
@@ -4,58 +4,6 @@ function selecCard(laCard) {
     this.losTickets();
 }    
 
-// function totalApagar(desc,cantidad) {
-//     if (cantidad == 0) {
-//         let cantidadDeTickets = document.getElementById('cant').value
-
-//         let precio = cantidadDeTickets * 200
-
-//         let descuentoAplicado = desc * precio / 100
-//         precio = precio - descuentoAplicado
-
-//         if (desc == 80) {
-//             document.getElementById('descSelec').innerHTML = "Estudiante"
-//             document.getElementById('descSelec').className = "text-primary"
-//         } else {
-//             if (desc == 50) {
-//                 document.getElementById('descSelec').innerHTML = "Trainee"
-//                 document.getElementById('descSelec').className = "text-success"
-//             } else {
-//                 if (desc == 15) {
-//                     document.getElementById('descSelec').innerHTML = "Junior"
-//                     document.getElementById('descSelec').className = "text-danger"  
-//                 } 
-//             }
-//         }
-
-//         document.getElementById('totalApagar').innerHTML = precio
-//     } else {
-//         let precio = cantidad * 200
-
-//         let descuentoAplicado = desc * precio / 100
-//         precio = precio - descuentoAplicado
-
-//         if (desc == 80) {
-//             document.getElementById('descSelec').innerHTML = "Estudiante"
-//             document.getElementById('descSelec').className = "text-primary"
-//         } else {
-//             if (desc == 50) {
-//                 document.getElementById('descSelec').innerHTML = "Trainee"
-//                 document.getElementById('descSelec').className = "text-success"
-//             } else {
-//                 if (desc == 15) {
-//                     document.getElementById('descSelec').innerHTML = "Junior"
-//                     document.getElementById('descSelec').className = "text-danger"  
-//                 } 
-//             }
-//         }
-
-//         document.getElementById('totalApagar').innerHTML = precio
-//     }
-    
-
-    
-// }
 
 function losTickets() {
     let cantidadTicket = document.getElementById('cant').value
@@ -64,4 +12,8 @@ function losTickets() {
     precio = precio - (descuentos * precio / 100)
 
     document.getElementById('totalApagar').innerHTML = precio
+}
+
+function borrarTodo() {
+    document.getElementById('totalApagar').innerHTML = 0
 }
